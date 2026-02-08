a46b129a75ebf37d2a5e3b62a89781b745d339d5
Joestarfish <130308219+Joestarfish@users.noreply.github.com>
2026-02-08 11:52:47 -0500

    Allow chaning a card title

--- src/CardHeader.tsx
+++ src/CardHeader.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react';
 import './CardHeader.css'
 
 function CardHeader() {
+  const DEFAULT_TITLE = "Title";
+
+  const [title, setTitle] = useState(DEFAULT_TITLE);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    // If the field was emptied we do not want to let it disappear
+    if (!editing && !title.trim()) {
+      setTitle(DEFAULT_TITLE);
+    }
+  }, [editing]);
+
   return (
     <div className="card-header">
-      <h3>Title</h3>
-      <button className="close-button" onClick={() => console.log("TODO: Add a close featuer")}>x</button>
+      {editing ? (
+        <input
+          autoFocus
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onBlur={() => setEditing(false)}
+          onKeyDown={(e) => e.key === "Enter" && setEditing(false)}
+        />
+      ) : (
+        <h3 onClick={() => setEditing(true)}>{title}</h3>
+      )}
+
+      <button className="close-button" onClick={() => console.log("TODO: Add a close feature")}>x</button>
     </div>
   )
 }
