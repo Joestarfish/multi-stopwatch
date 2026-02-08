21ad6569b59276784589e465a301941f7de0c128
Joestarfish <130308219+Joestarfish@users.noreply.github.com>
2026-02-08 12:32:58 -0500

    Add reset button

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,13 +2,14 @@ import { useEffect, useRef, useState } from 'react'
 import { Duration } from 'luxon';
 import './Card.css'
 import CardHeader from './CardHeader';
+import { RiResetLeftLine } from 'react-icons/ri';
 
 function Card() {
   const [isTicking, setIsTicking] = useState(false)
   const [numberOfMs, setCurrentNumberOfMs] = useState(0);
+  const [formattedTime, setFormattedTime] = useState("00:00:00.000");
   const interval = useRef<number>(null);
   const lastTick = useRef<Date>(null);
-  const formattedTime = useRef("00:00:00.000");
 
   useEffect(() => {
     if (!isTicking && interval.current !== null) {
@@ -29,20 +30,33 @@ function Card() {
   }, [isTicking]);
 
   useEffect(() => {
-    formattedTime.current = Duration.fromMillis(numberOfMs).toFormat("hh:mm:ss.SSS");
+    setFormattedTime(Duration.fromMillis(numberOfMs).toFormat("hh:mm:ss.SSS"));
   }, [numberOfMs]);
 
   return (
     <>
       <div className="card">
 
-      <CardHeader />
-       
+        <CardHeader />
+
         <div className="stopwatch-container">
           <pre>
-            <p>{formattedTime.current}</p>
+            <p>{formattedTime}</p>
           </pre>
-          <button onClick={() => setIsTicking(!isTicking)}>{isTicking ? 'Pause' : 'Start'}</button>
+
+          <div className="buttons-container">
+            <button
+              className="pause"
+              onClick={() => setIsTicking(!isTicking)}>
+              {isTicking ? 'Pause' : 'Start'}
+            </button>
+
+            <button
+              className="reset"
+              onClick={() => { setIsTicking(false); setCurrentNumberOfMs(0); }}>
+              <RiResetLeftLine />
+            </button>
+          </div>
         </div>
       </div>
     </>
